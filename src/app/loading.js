2932c2427a5115da69e9f25140c6bb6891a52dd1
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-12-06 23:14:56 +0600

    update lodaing

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,7 +4,7 @@ const LoadingPage = () => {
     return (
         <div className='  flex h-screen 
           justify-center items-center'>
-            <span className="loading  h-24 w-24 loading-spinner text-error"></span>
+            <span className="loading  h-24 w-28 loading-spinner text-error"></span>
         </div>
     );
 };
